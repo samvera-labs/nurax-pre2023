88d62ee274b9505865cdc0faa98d80e6270550bc
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-09-01 15:35:53 -0700

    Allow 0-file works to be deposited
    Refs #8

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -108,7 +108,7 @@ Hyrax.config do |config|
   # Should work creation require file upload, or can a work be created first
   # and a file added at a later time?
   # The default is true.
-  # config.work_requires_files = true
+  config.work_requires_files = false
 
   # Should a button with "Share my work" show on the front page to all users (even those not logged in)?
   # config.display_share_button_when_not_logged_in = true
