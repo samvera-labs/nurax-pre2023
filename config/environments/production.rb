a93f77c8a77702e5fb68c6453c5d870001537451
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-09-01 15:45:20 -0700

    Use Sidekiq for background jobs
    Sets up sidekiq for processing background jobs in dev and prod environments. Configures two queues by default. Allows for setting Redis host and port by environment variable.
    
    Refs #8

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Use Sidekiq to process background jobs
+  config.active_job.queue_adapter = :sidekiq
+
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
