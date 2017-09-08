7367502097ad4fa71fa12f007b5464e4e87facef
Alicia Cozine <alicia@curationexperts.com>
2017-09-07 22:48:54 -0500

    adds travis CI, CD when master branch succeeds

---
+++ config/deploy/travis.rb
@@ -0,0 +1,7 @@
+# deploys to DCE sandbox
+set :stage, :travis
+set :rails_env, 'production'
+set :deploy_to, '/opt/nurax'
+server 'demo.curationexperts.com', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
+set :ssh_options, keys: ['config/nurax-travis']
+
