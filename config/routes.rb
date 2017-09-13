8dac1cfa5faed153288f191f8c7a753b0b74ccba
Alicia Cozine <alicia@curationexperts.com>
2017-09-13 15:07:39 -0500

    adds sidekiq dashboard, reorganizes routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,34 @@
 Rails.application.routes.draw do
-  mount BrowseEverything::Engine => '/browse'
-
   mount Blacklight::Engine => '/'
+  mount BrowseEverything::Engine => '/browse'
+  mount Hyrax::Engine, at: '/'
+  mount Qa::Engine => '/authorities'
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
 
+  concern :exportable, Blacklight::Routes::Exportable.new
   concern :searchable, Blacklight::Routes::Searchable.new
+
+  devise_for :users
+  curation_concerns_basic_routes
+
+  resources :bookmarks do
+    concerns :exportable
+    collection do
+      delete 'clear'
+    end
+  end
 
   resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
     concerns :searchable
   end
 
-  devise_for :users
-  mount Qa::Engine => '/authorities'
-  mount Hyrax::Engine, at: '/'
-  resources :welcome, only: 'index'
-  root 'hyrax/homepage#index'
-  curation_concerns_basic_routes
-  concern :exportable, Blacklight::Routes::Exportable.new
-
   resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
     concerns :exportable
   end
 
-  resources :bookmarks do
-    concerns :exportable
-
-    collection do
-      delete 'clear'
-    end
-  end
+  resources :welcome, only: 'index'
+  root 'hyrax/homepage#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
