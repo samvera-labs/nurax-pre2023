4773a6227b659abc2caa06a08ad5041961bc37ea
Julie Allinson <julie.allinson@london.ac.uk>
2018-02-26 22:03:02 +0000

    set public_file_server to true in production config, see https://github.com/pulibrary/pul_uv_rails/issues/8

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # pul_uv_rails requires this to be true
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
