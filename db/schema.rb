d7708dda10c8b8fad6c48c4690517fbe01927036
E. Lynette Rayle <elr37@cornell.edu>
2018-03-12 09:33:04 -0400

    update to hyrax branch v2.1.0_beta1a_qa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219230906) do
+ActiveRecord::Schema.define(version: 20180312130601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(version: 20171219230906) do
     t.boolean "assigns_workflow", default: false, null: false
     t.boolean "assigns_visibility", default: false, null: false
     t.boolean "share_applies_to_new_works", default: true, null: false
+    t.boolean "brandable", default: true, null: false
     t.index ["machine_id"], name: "index_hyrax_collection_types_on_machine_id", unique: true
   end
 
