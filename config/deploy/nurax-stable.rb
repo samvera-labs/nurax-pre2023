2eaf84a1bd702de2c532cbbaf6edcc665316735d
Bess Sadler <bess@curationexperts.com>
2018-07-24 10:01:46 -0400

    Deploy to nurax-stable
    * Remove unused deploy targets
    * Add deploy target for nurax-stable
    * Pin hyrax version to 2.1.0

---
+++ config/deploy/nurax-stable.rb
@@ -0,0 +1,1 @@
+server 'nurax-stable.curationexperts.com', user: 'deploy', roles: [:web, :app, :db]
--- config/deploy/sandbox.rb
+++
@@ -1,5 +0,0 @@
-# deploys to DCE sandbox
-set :stage, :sandbox
-set :rails_env, 'production'
-set :deploy_to, '/opt/nurax'
-server 'nurax.curationexperts.com', user: 'deploy', roles: [:web, :app, :db]
