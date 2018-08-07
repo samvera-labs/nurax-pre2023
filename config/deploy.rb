dd4e39bf22ad319d918ff1238344a2d65aa51fdf
Bess Sadler <bess@curationexperts.com>
2018-08-07 08:45:38 -0400

    Keep database.yml as-is. Let's change one thing at a time.

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :branch, ENV['REVISION'] || ENV['BRANCH'] || ENV['BRANCH_NAME'] || 'master'
 # Default value for :linked_files is []
 append :linked_files, "config/analytics.yml"
 # append :linked_files, "config/browse_everything_providers.yml"
-# append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml"
 append :linked_files, "config/secrets.yml"
 append :linked_files, ".env.production"
 
