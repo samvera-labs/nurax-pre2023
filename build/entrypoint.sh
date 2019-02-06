7956dfa4d6bd7f702f94f61d22399e836f9cb17c
Josh Gum <revgum@gmail.com>
2019-02-06 10:36:59 -0800

    disable solr script integration

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -8,9 +8,8 @@ rm -f tmp/puma.pid
 # Guarantee gems are installed in case docker image is outdated
 ./build/install_gems.sh
 
-# Do not auto-create SOLR or Migrations for production or staging environments
+# Do not auto-migrate for production or staging environments
 if [ "${RAILS_ENV}" != 'production' ] && [ "${RAILS_ENV}" != 'staging' ]; then
-  ./build/create_solr_index.sh
   ./build/validate_migrated.sh
 fi
 
