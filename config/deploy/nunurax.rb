4d1aa5a2ae3410e74a8459ebe87e2f0af8bf1bc3
Bess Sadler <bess@curationexperts.com>
2019-09-25 14:49:58 -0400

    Update deploy targets

--- config/deploy/nunurax.rb
+++ config/deploy/nunurax.rb
@@ -1,5 +1,1 @@
-# deploys to DCE sandbox
-set :stage, :sandbox
-set :rails_env, 'production'
-set :deploy_to, '/opt/nurax'
 server 'nunurax.curationexperts.com', user: 'deploy', roles: [:web, :app, :db]
--- config/deploy/travis.rb
+++
@@ -1,6 +0,0 @@
-# deploys to DCE sandbox
-set :stage, :travis
-set :rails_env, 'production'
-set :deploy_to, '/opt/nurax'
-server 'nurax.curationexperts.com', user: 'deploy', roles: [:web, :app, :db]
-set :ssh_options, keys: ['config/nurax-travis']
