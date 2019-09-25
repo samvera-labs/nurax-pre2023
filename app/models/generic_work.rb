ad140f4f5926c4a81043ab9b015ddcdf04fef9fc
Bess Sadler <bess@curationexperts.com>
2019-09-25 15:22:24 -0400

    Remove deprecated human_readable_type attribute

--- app/models/generic_work.rb
+++ app/models/generic_work.rb
@@ -8,8 +8,6 @@ class GenericWork < ActiveFedora::Base
   # self.valid_child_concerns = []
   validates :title, presence: { message: 'Your work must have a title.' }
 
-  self.human_readable_type = 'Generic Work'
-
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
