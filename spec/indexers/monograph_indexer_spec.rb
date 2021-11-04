8d2b2e00415902a346f85300fef6cb767269c913
E. Lynette Rayle <elr37@cornell.edu>
2021-11-04 16:01:57 -0400

    add monograph valkyrie based work type

---
+++ spec/indexers/monograph_indexer_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Generated via
+#  `rails generate hyrax:work_resource Monograph`
+require 'rails_helper'
+require 'hyrax/specs/shared_specs/indexers'
+
+RSpec.describe MonographIndexer do
+  let(:indexer_class) { described_class }
+
+  it_behaves_like 'a Hyrax::Resource indexer'
+end
---
+++ spec/models/monograph_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Generated via
+#  `rails generate hyrax:work_resource Monograph`
+require 'rails_helper'
+require 'hyrax/specs/shared_specs/hydra_works'
+
+RSpec.describe Monograph do
+  subject(:work) { described_class.new }
+
+  it_behaves_like 'a Hyrax::Work'
+end
