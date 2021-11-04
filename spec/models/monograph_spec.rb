bd93a7b99a65ab6db5bc370717c637726fe9d62d
E. Lynette Rayle <elr37@cornell.edu>
2021-11-04 17:00:42 -0400

    monograph as generated fails to set :resource which is required for shared specs

--- spec/models/monograph_spec.rb
+++ spec/models/monograph_spec.rb
@@ -6,7 +6,8 @@ require 'rails_helper'
 require 'hyrax/specs/shared_specs/hydra_works'
 
 RSpec.describe Monograph do
-  subject(:work) { described_class.new }
+  subject(:monograph) { described_class.new }
+  let(:resource) { monograph }
 
   it_behaves_like 'a Hyrax::Work'
 end
