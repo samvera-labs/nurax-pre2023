089350d98ea9fd529aa9fbb91af9e36a6e46d2ee
Daniel Pierce <dlpierce@indiana.edu>
2021-12-10 16:20:19 -0500

    Fix docker-compose with updated images (#406)

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -8,6 +8,8 @@ rm -f tmp/puma.pid
 # Guarantee gems are installed in case docker image is outdated
 ./build/install_gems.sh
 
+yarn install
+
 # Do not auto-migrate for production or staging environments
 if [ "${RAILS_ENV}" != 'production' ] && [ "${RAILS_ENV}" != 'staging' ]; then
   ./build/validate_migrated.sh
